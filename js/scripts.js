b11338134837bb3d9d65b8407353daa6e0cf9735
chris martinez and Timothy Bourgault <timothy@>
2016-08-09 09:59:59 -0700

    complete walruss project

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(function(){
+
+
+  $("#fadeIn").click(function(){
+    $("#walrus").fadeIn();
+  });
+
+  $("#fadeOut").click(function(){
+    $("#walrus").fadeOut();
+  });
+  $("#slideUp").click(function(){
+    $("#walrus").slideUp();
+  });
+  $("#slideDown").click(function(){
+    $("#walrus").slideDown();
+  });
+  $("#slideToggle").click(function(){
+    $("#walrus").slideToggle();
+  });
+
+
+
+
+
+});
